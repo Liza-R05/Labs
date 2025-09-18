5bf62eb99d70303052e84ebb59812897d99a9709
Elizaveta Rassokha <lenta_sv@mail.ru>
2025-09-18 15:06:53 +0300

    Source.c

--- task3/Source.c
+++ task3/Source.c
@@ -4,7 +4,7 @@ int main()
 {
 	setlocale(LC_ALL, "RUS");
 	int L = 1323, n = 2;
-	printf("Дано:\n \t%d\n \t%4.0d\n ", L, n);
+	printf("Р”Р°РЅРѕ:\n \t%d\n \t%4.0d\n ", L, n);
 	printf("\t_______");
-	printf("\nОтвет:\n \t+%011.7f\n ", 2.0 / 1323);
+	printf("\nРћС‚РІРµС‚:\n \t+%011.7f\n ", 2.0 / 1323);
 }
