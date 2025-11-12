305dc3289425aa049f20ead4144411e8c6d872ad
Elizaveta Rassokha <lenta_sv@mail.ru>
2025-11-12 15:25:48 +0300

    Update task1.c

--- Lab-16/task1.c
+++ Lab-16/task1.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <locale.h>
-// из функции зад.2 лаб.8
+// РёР· С„СѓРЅРєС†РёРё Р·Р°Рґ.2 Р»Р°Р±.8
 double* full_elements(double* ptr_array, int n) 
 {
     for (int i = 0; i < n; i++) 
@@ -12,7 +12,7 @@ double* full_elements(double* ptr_array, int n)
     }
     return ptr_array;
 }
-// печать элементов массива
+// РїРµС‡Р°С‚СЊ СЌР»РµРјРµРЅС‚РѕРІ РјР°СЃСЃРёРІР°
 int put_elements(double* ptr_array, int n) 
 {
     for (int i = 0; i < n; i++) 
@@ -21,7 +21,7 @@ int put_elements(double* ptr_array, int n)
     }
     return n;
 }
-// обработка элементов массива (возведение в квадрат - вариант 4)
+// РѕР±СЂР°Р±РѕС‚РєР° СЌР»РµРјРµРЅС‚РѕРІ РјР°СЃСЃРёРІР° (РІРѕР·РІРµРґРµРЅРёРµ РІ РєРІР°РґСЂР°С‚ - РІР°СЂРёР°РЅС‚ 4)
 double* calc_elements(double* ptr_array, int n) 
 {
     for (int i = 0; i < n; i++) 
@@ -35,7 +35,7 @@ void main()
     setlocale(LC_ALL, "RUS");
     double* ptr_array;
     int size;
-    printf("Введите размер массива: ");
+    printf("Р’РІРµРґРёС‚Рµ СЂР°Р·РјРµСЂ РјР°СЃСЃРёРІР°: ");
     scanf("%d", &size);
     ptr_array = (double*)malloc(size * sizeof(double));
     if (ptr_array == NULL) 
@@ -56,9 +56,9 @@ void main()
         result_array[i] = ptr_array[i];
     }
     calc_elements(result_array, size);
-    printf("\nИсходный массив:\n");
+    printf("\nРСЃС…РѕРґРЅС‹Р№ РјР°СЃСЃРёРІ:\n");
     put_elements(ptr_array, size);
-    printf("\nПреобразованный массив:\n");
+    printf("\nРџСЂРµРѕР±СЂР°Р·РѕРІР°РЅРЅС‹Р№ РјР°СЃСЃРёРІ:\n");
     put_elements(result_array, size);
     free(ptr_array);
     free(result_array);
