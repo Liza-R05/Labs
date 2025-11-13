624dec7adfa3e67319ad5482e6f2f33e8ef0c632
Elizaveta Rassokha <lenta_sv@mail.ru>
2025-11-13 18:54:54 +0300

    Update task2.2.c

--- Lab-16/task2.2.c
+++ Lab-16/task2.2.c
@@ -4,60 +4,61 @@
 #include <locale.h>
 
 // заполнение массива случайными числами от -1 до 1
-double* full_elements(double* ptr_array, int n) 
+double* full_elements(double* ptr_array, int n)
 {
-    for (int i = 0; i < n; i++) 
+    for (int i = 0; i < n; i++)
     {
         ptr_array[i] = -1.0 + 2.0 * rand() / RAND_MAX;
     }
     return ptr_array;
 }
+
 // печать элементов массива
-int put_elements(double* ptr_array, int n) 
+int put_elements(double* ptr_array, int n)
 {
-    for (int i = 0; i < n; i++) 
+    for (int i = 0; i < n; i++)
     {
         printf("[%d] = %.2f\n", i, ptr_array[i]);
     }
     return n;
 }
-// вставка -999 перед максимальным элементом
-double* insert_before_max(double* ptr_array, int* n) 
+
+// вставка -999 перед максимальным элементом (по образцу из картинки)
+double* insert_before_max(double* ptr_arr, int* size)
 {
-    if (*n == 0) return ptr_array;
-    // индекс максимального элемента
-    int max_index = 0;
-    for (int i = 1; i < *n; i++) 
+    if (*size == 0) return ptr_arr;
+    // находим индекс максимального элемента
+    int k = 0;
+    for (int i = 1; i < *size; i++)
     {
-        if (ptr_array[i] > ptr_array[max_index]) 
+        if (ptr_arr[i] > ptr_arr[k])
         {
-            max_index = i;
+            k = i;
         }
     }
-    // увеличиваем на 1 элемент
-    double* new_array = (double*)malloc((*n + 1) * sizeof(double));
-    if (new_array == NULL) 
+    // увеличиваем размер массива
+    int size_n = (*size) + 1;
+    double* ptr_arr_n = (double*)realloc(ptr_arr, size_n * sizeof(double));
+    if (ptr_arr_n == NULL)
     {
         printf("error\n");
-        return ptr_array;
+        return ptr_arr;
     }
-    // копируем элементы до максимального
-    for (int i = 0; i < max_index; i++) 
+    ptr_arr = ptr_arr_n;
+    // сдвигаем элементы для вставки
+    for (int i = size_n - 1; i > k; i--)
     {
-        new_array[i] = ptr_array[i];
+        ptr_arr[i] = ptr_arr[i - 1];
     }
-    new_array[max_index] = -999.0;
-    // остальные элементы
-    for (int i = max_index; i < *n; i++) 
-    {
-        new_array[i + 1] = ptr_array[i];
-    }
-    (*n)++;
-    free(ptr_array);
-    return new_array;
+    // вставляем -999
+    double num = -999.0;
+    ptr_arr[k] = num;
+    *size = size_n;
+
+    return ptr_arr;
 }
 
-void main() 
+void main()
 {
     setlocale(LC_ALL, "RUS");
     double* ptr_array;
@@ -65,7 +66,7 @@ void main()
     printf("Введите размер массива: ");
     scanf("%d", &size);
     ptr_array = (double*)malloc(size * sizeof(double));
-    if (ptr_array == NULL) 
+    if (ptr_array == NULL)
     {
         printf("error\n");
         return -1;
@@ -79,3 +80,4 @@ void main()
     free(ptr_array);
     system("pause");
 }
+
